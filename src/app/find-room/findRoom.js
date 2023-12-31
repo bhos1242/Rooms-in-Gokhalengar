85ec6e30c4a5648b2aab31dcaef7c723582a155c
vivek bhos <bhosvivek123@gmail.com>
2023-12-31 14:13:18 +0530

    updated homepage

--- src/app/find-room/findRoom.js
+++ src/app/find-room/findRoom.js
@@ -30,32 +30,38 @@ const FindRoom = () => {
       <h2 className="text-4xl font-semibold mb-6 text-center text-blue-500">Find a Room</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {roomDetails.map((room) => (
-          <div key={room._id} className="mb-6 p-4 border border-gray-300 rounded-md shadow-md">
-            <h3 className="text-xl font-semibold mb-2">{room.title}</h3>
-            <p className="text-gray-600 mb-4">{room.description}</p>
-            <div className="flex flex-col mb-4">
-              <span className="font-semibold text-gray-800">Rent:</span>
-              <span className="text-gray-600">{`$${room.rent}`}</span>
+          <div key={room._id} className="bg-white rounded-lg overflow-hidden shadow-md">
+            <img
+              src="room-image-placeholder.jpg" // Replace with actual image URL or use room images from your server
+              alt={`Room: ${room.title}`}
+              className="w-full h-48 object-cover md:h-64"
+            />
+            <div className="p-4">
+              <h3 className="text-xl font-semibold mb-2">{room.title}</h3>
+              <p className="text-gray-600 mb-4">{room.description}</p>
+              <div className="flex flex-col mb-4">
+                <span className="font-semibold text-gray-800">Rent:</span>
+                <span className="text-gray-600">{`Rs${room.rent}`}</span>
+              </div>
+              <div className="flex flex-col mb-4">
+                <span className="font-semibold text-gray-800">Location:</span>
+                <span className="text-gray-600">{room.location}</span>
+              </div>
+              <div className="flex flex-col mb-4">
+                <span className="font-semibold text-gray-800">Accommodation Type:</span>
+                <span className="text-gray-600">{room.accommodationType}</span>
+              </div>
+              <div className="flex flex-col mb-4">
+                <span className="font-semibold text-gray-800">Contact Info:</span>
+                <span className="text-gray-600">{room.contactInfo}</span>
+              </div>
+              <div className="flex flex-col">
+                <span className="font-semibold text-gray-800">Light Bill Included:</span>
+                <span className={`text-${room.lightBillIncluded ? 'green' : 'red'}-600`}>
+                  {room.lightBillIncluded ? 'Yes' : 'No'}
+                </span>
+              </div>
             </div>
-            <div className="flex flex-col mb-4">
-              <span className="font-semibold text-gray-800">Location:</span>
-              <span className="text-gray-600">{room.location}</span>
-            </div>
-            <div className="flex flex-col mb-4">
-              <span className="font-semibold text-gray-800">Accommodation Type:</span>
-              <span className="text-gray-600">{room.accommodationType}</span>
-            </div>
-            <div className="flex flex-col mb-4">
-              <span className="font-semibold text-gray-800">Contact Info:</span>
-              <span className="text-gray-600">{room.contactInfo}</span>
-            </div>
-            <div className="flex flex-col">
-              <span className="font-semibold text-gray-800">Light Bill Included:</span>
-              <span className={`text-${room.lightBillIncluded ? 'green' : 'red'}-600`}>
-                {room.lightBillIncluded ? 'Yes' : 'No'}
-              </span>
-            </div>
-            {/* Display other room details as needed */}
           </div>
         ))}
       </div>
