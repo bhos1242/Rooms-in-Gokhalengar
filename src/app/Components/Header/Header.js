093dab48e73ea3e2979b9b6582659522a0b87d48
vivek1242 <bhosvivek123@gmail.com>
2024-01-10 17:29:42 +0530

    message menu hide

--- src/app/Components/Header/Header.js
+++ src/app/Components/Header/Header.js
@@ -1,10 +1,10 @@
 // Import necessary modules and styles
-"use client"
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import Link from 'next/link';
-import Image from 'next/image';
-import logo from "../../assets/logos2/logo-no-background.png"
+"use client";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../assets/logos2/logo-no-background.png";
 
 export default function Header() {
   // Use the useSession hook to retrieve user session information
@@ -25,45 +25,54 @@ export default function Header() {
 
   return (
     <nav className="bg-white border-b border-gray-200 dark:bg-gray-900 lg:px-8 ">
-      <div className="container mx-auto flex items-center justify-between py-5 px-3">
+      <div className="container flex items-center justify-between px-3 py-5 mx-auto">
         <Link href="/">
-          <p className="flex items-center space-x-3 cursor-pointer h-6">
-            <Image
-              src={logo}
-              alt="Flowbite Logo"
-              width={200}
-              height={200}
-            />
+          <p className="flex items-center h-6 space-x-3 cursor-pointer">
+            <Image src={logo} alt="Flowbite Logo" width={200} height={200} />
           </p>
         </Link>
 
         {/* Desktop menu */}
-        <div className="hidden md:flex items-center space-x-4">
-          <NavLink href="/" onClick={closeMobileMenu}>Home</NavLink>
-          <NavLink href="/post-vacancy" onClick={closeMobileMenu}>Post Vacancy</NavLink>
-          <NavLink href="/find-room" onClick={closeMobileMenu}>Find Room</NavLink>
-          <NavLink href="/messages" onClick={closeMobileMenu}>Messages</NavLink>
+        <div className="items-center hidden space-x-4 md:flex">
+          <NavLink href="/" onClick={closeMobileMenu}>
+            Home
+          </NavLink>
+          <NavLink href="/post-vacancy" onClick={closeMobileMenu}>
+            Post Vacancy
+          </NavLink>
+          <NavLink href="/find-room" onClick={closeMobileMenu}>
+            Find Room
+          </NavLink>
+          {/* <NavLink href="/messages" onClick={closeMobileMenu}>
+            Messages
+          </NavLink> */}
 
           {/* Conditional rendering based on authentication status */}
-          {session.status === 'unauthenticated' && (
+          {session.status === "unauthenticated" && (
             <>
-              <NavLink href="/login" onClick={closeMobileMenu}>Login</NavLink>
-              <NavLink href="/resister" onClick={closeMobileMenu}>Sign Up</NavLink>
+              <NavLink href="/login" onClick={closeMobileMenu}>
+                Login
+              </NavLink>
+              <NavLink href="/resister" onClick={closeMobileMenu}>
+                Sign Up
+              </NavLink>
             </>
           )}
-          {session.status === 'authenticated' && (
-            <NavLink href="/logout" onClick={closeMobileMenu}>Logout</NavLink>
+          {session.status === "authenticated" && (
+            <NavLink href="/logout" onClick={closeMobileMenu}>
+              Logout
+            </NavLink>
           )}
         </div>
 
         {/* Mobile menu button */}
         <button
           onClick={toggleMobileMenu}
-          className="md:hidden text-gray-500 focus:outline-none"
+          className="text-gray-500 md:hidden focus:outline-none"
           aria-label="Open Menu"
         >
           <svg
-            className={`w-6 h-6 ${isMobileMenuOpen ? 'hidden' : 'block'}`}
+            className={`w-6 h-6 ${isMobileMenuOpen ? "hidden" : "block"}`}
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -77,7 +86,7 @@ export default function Header() {
             />
           </svg>
           <svg
-            className={`w-6 h-6 ${isMobileMenuOpen ? 'block' : 'hidden'}`}
+            className={`w-6 h-6 ${isMobileMenuOpen ? "block" : "hidden"}`}
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -96,24 +105,42 @@ export default function Header() {
       {/* Mobile menu */}
       <div
         className={`md:hidden ${
-          isMobileMenuOpen ? 'block' : 'hidden'
+          isMobileMenuOpen ? "block" : "hidden"
         } bg-white border-t border-gray-200 dark:bg-gray-900`}
       >
-        <div className="container mx-auto p-4">
-          <NavLink href="/" onClick={closeMobileMenu}>Home</NavLink>
-          <NavLink href="/post-vacancy" onClick={closeMobileMenu}>Post Vacancy</NavLink>
-          <NavLink href="/find-room" onClick={closeMobileMenu}>Find Room</NavLink>
-          <NavLink href="/messages" onClick={closeMobileMenu}>Messages</NavLink>
+        <div className="container p-4 mx-auto">
+          <NavLink href="/" onClick={closeMobileMenu}>
+            Home
+          </NavLink>
+          <NavLink href="/post-vacancy" onClick={closeMobileMenu}>
+            Post Vacancy
+          </NavLink>
+          <NavLink href="/find-room" onClick={closeMobileMenu}>
+            Find Room
+          </NavLink>
+          {/* <NavLink href="/messages" onClick={closeMobileMenu}>
+            Messages
+          </NavLink> */}
 
           {/* Conditional rendering based on authentication status */}
-          {session.status === 'unauthenticated' && (
+          {session.status === "unauthenticated" && (
             <>
-              <NavLink href="/login" onClick={closeMobileMenu}>Login</NavLink>
-              <NavLink href="/resister" onClick={closeMobileMenu}>Sign Up</NavLink>
+              <NavLink href="/login" onClick={closeMobileMenu}>
+                Login
+              </NavLink>
+              <NavLink href="/resister" onClick={closeMobileMenu}>
+                Sign Up
+              </NavLink>
             </>
           )}
-          {session.status === 'authenticated' && (
-            <NavLink href="/logout" onClick={closeMobileMenu}>Logout</NavLink>
+          {session.status === "authenticated" && (
+            <NavLink
+              href="/logout"
+              className="p-4 bg-red-600"
+              onClick={closeMobileMenu}
+            >
+              Logout
+            </NavLink>
           )}
         </div>
       </div>
@@ -125,7 +152,10 @@ export default function Header() {
 function NavLink({ href, children, onClick }) {
   return (
     <Link href={href} passHref>
-      <p onClick={onClick} className="text-gray-700 dark:text-white hover:text-blue-500 cursor-pointer py-2">
+      <p
+        onClick={onClick}
+        className="py-2 text-gray-700 cursor-pointer dark:text-white hover:text-blue-500"
+      >
         {children}
       </p>
     </Link>
